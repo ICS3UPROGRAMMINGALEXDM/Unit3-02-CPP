78cda71354a4607e5268cb2abe3056108b757649
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-03-29 13:47:00 +0000

    started

---
+++ guessANum.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 Alex De Meo All rights reserved
+//
+// Created By: Alex De Meo
+// Date: 02//22
+// Description: In this program you will have to guess a randomly generated number correctly
+#include <iostream>
+#include <random>
+#include <stdlib.h>
+using std::cout;
+using std::cin;
+using std::string;
+
+int main()
+{
+	int rNum, uNum;
+	bool keepPlaying = true, answerincorrect = true;
+	
+	while (keepPlaying ?){
+	    rNum = rand
+	}
+}
